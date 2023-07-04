7f597e3dfd4d8c0904e33ef1c8357b920d877524
Shaikh Saqlain Ahmad <98037915+Shaikh-Saqlain-Ahmad@users.noreply.github.com>
2023-07-04 21:43:03 +0500

    Create customTheme.dart

---
+++ customTheme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+final themeData=ThemeData(
+  textTheme:TextTheme(
+    bodySmall: TextStyle(
+      color: Colors.red,
+      fontSize: 11
+    )
+  )
+
+);
