4923b5aeb08d60387ed73897a704f82b2e024b4a
Shaikh Saqlain Ahmad <98037915+Shaikh-Saqlain-Ahmad@users.noreply.github.com>
2023-07-06 21:58:37 +0500

    Update customTheme.dart

--- customTheme.dart
+++ customTheme.dart
@@ -6,6 +6,15 @@ final customTheme=ThemeData(
     enabledBorder:OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color:Colors.green)),
     focusedErrorBorder:OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color:Colors.orange)),
     disabledBorder: OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color:Colors.grey)) 
-  )
-
+  ),
 );
+final darkTheme=ThemeData(
+  inputDecorationTheme: InputDecorationTheme(
+    focusedBorder: OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color: Colors.white)),
+    errorBorder:OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color: Colors.red)),
+    enabledBorder:OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color:Colors.pink)),
+    focusedErrorBorder:OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color:Colors.orange)),
+    disabledBorder: OutlineInputBorder(borderRadius:BorderRadius.circular(20),borderSide: BorderSide(color:Colors.grey)) 
+  )
+  //primarySwatch: MaterialColor(000000,) 
+  );
